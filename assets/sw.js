5a46251a776d99b503974651374bc3bd5fd21898
JarnoEetSoep <romijnjarno@gmail.com>
2026-05-20 20:45:39 +0200

    Updated serviceworker

--- assets/sw.js
+++ assets/sw.js
@@ -1,25 +1,36 @@
-var cacheName = 'territory';
+var cacheName = "territory-1779302640";
 var filesToCache = [
-  './',
-  './index.html',
-  './territory.js',
-  './territory_bg.wasm',
+  "./",
+  "./index.html",
+  "./territory.js",
+  "./territory_bg.wasm",
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
+/* Start the service worker and cache all of the app"s content */
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(cache => {
       return cache.addAll(filesToCache);
     })
   );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(function (response) {
+    caches.match(e.request).then(response => {
       return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(async keys => {
+      Promise.all(keys
+        .filter(key => key != cacheName)
+        .map(async key => await caches.delete(key))
+      )
     })
   );
 });
